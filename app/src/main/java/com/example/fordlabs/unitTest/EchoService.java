4af0a5e57176fe4832c89eb43fc6df410971dfea
faizal <sfaizalfarhaan@gmail.com>
2019-08-07 18:27:17 +0530

    Echo & warning updated test cases

--- app/src/main/java/com/example/fordlabs/unitTest/EchoService.java
+++ app/src/main/java/com/example/fordlabs/unitTest/EchoService.java
@@ -1,19 +1,75 @@
 package com.example.fordlabs.unitTest;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EchoService {
 
-    public String ping(String message) {
+    public String ping(String ... messageArgs) {
 
-        String pingMessage = null;
+        String pingMessage = "";
+        boolean isInvalidInput = false;
 
-        if (message != null && message.equals(message.toUpperCase())) {
-            pingMessage = "WARNING:" + message;
-        } else if (message != null && !message.equals(message.toUpperCase())) {
-            pingMessage = "Echo:" + message;
+        List<String>warningsList = new ArrayList<String>();
+        List<String>echoList = new ArrayList<String>();
 
-        } else {
-            pingMessage = "There is nothing to echo";
+        for(String message : messageArgs ){
+            if (message != null && message.length() > 0 && message.equals(message.toUpperCase())) {
+                warningsList.add(message);
+            } else if (message != null && message.length() > 0 &&  !message.equals(message.toUpperCase())) {
+                echoList.add(message);
+
+            } else {
+                pingMessage = "There is nothing to echo";
+                isInvalidInput= true;
+                break;
+            }
         }
+
+
+
+        if(!isInvalidInput){
+
+            if(echoList.size() ==1){
+                pingMessage = "Echo:"+" "+echoList.get(0);
+            }
+            else if(!echoList.isEmpty() && echoList.size() >1){
+
+                StringBuilder sb = new StringBuilder();
+                for(int i=0;i<echoList.size()-1;i++){
+                  sb =   sb.length() > 0  ? sb.append(","+echoList.get(i)) : sb.append(echoList.get(i));
+
+                }
+                sb.append(" "+"and"+" ");
+                sb.append(echoList.get(echoList.size()-1)+".");
+                pingMessage = "Echo:"+sb.toString();
+
+            }
+            if(warningsList.size() == 1){
+                pingMessage = "WARNING:"+" "+warningsList.get(0);
+            }
+
+            else if(!warningsList.isEmpty() && warningsList.size() >1){
+
+                StringBuilder sb2 = new StringBuilder();
+
+                for(int i=0;i<warningsList.size()-1;i++){
+                    sb2  = sb2.length() > 0 ? sb2.append(","+warningsList.get(i)) : sb2.append(warningsList.get(i));
+                }
+                sb2.append(" "+"AND"+" ");
+                sb2.append(warningsList.get(warningsList.size()-1)+".");
+                pingMessage = pingMessage+"WARNING:"+sb2.toString();
+            }
+        }
+
         return pingMessage;
     }
+
+    public void logMessage(){
+        Log.i("EchoService_LOG", "logMessage: Logging Message from echoService");
+        Log.i("TAG",this.getClass().toString());
+    }
+    
 }
--- app/src/test/java/com/example/fordlabs/unitTest/EchoTest.java
+++ app/src/test/java/com/example/fordlabs/unitTest/EchoTest.java
@@ -2,18 +2,19 @@ package com.example.fordlabs.unitTest;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 
 public class EchoTest {
 
     EchoService echoService = new EchoService();
 
+
     //case 1
     @Test
     public void testPing() {
         String message = "starfish";
-        assertEquals("Echo:starfish", echoService.ping(message));
+        assertEquals("Echo: starfish", echoService.ping(message));
     }
 
     //case 2
@@ -27,9 +28,25 @@ public class EchoTest {
     @Test
     public void testPingForUpperCase() {
         String message = "SHARK";
-        assertEquals("WARNING:SHARK", echoService.ping(message));
+        assertEquals("WARNING: SHARK", echoService.ping(message));
     }
 
+
+    @Test
+    public void testPingForVarargs(){
+        assertEquals("Echo:lobster and fish.WARNING:SHARK AND SEASNAKE.",echoService.ping("lobster","SHARK","fish","SEASNAKE"));
+    }
+
+
+    @Test
+    public void testPingForVarargsWithEcho(){
+        assertEquals("Echo:lobster and fish.",echoService.ping("lobster","fish"));
+    }
+
+    @Test
+    public void testPingForVarargsWithWarning(){
+        assertEquals("WARNING:SHARK AND SEASNAKE.",echoService.ping("SHARK","SEASNAKE"));
+    }
 
 
 }
